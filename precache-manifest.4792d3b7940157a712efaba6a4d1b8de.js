7105e2508b53561bbd88868b07b99faf7d19acfe
user.name : "Ashwani Yadav <user.email : "ashwaniyadav888@gmail.com>
2024-06-23 08:04:48 +0530

    Updates

---
+++ precache-manifest.4792d3b7940157a712efaba6a4d1b8de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a11742091d07b7151b90b844e0e535d",
+    "url": "/Text-Zone/index.html"
+  },
+  {
+    "revision": "af946713adc6107d8806",
+    "url": "/Text-Zone/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "c72feb6dc286339a449b",
+    "url": "/Text-Zone/static/js/2.38fec76d.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/Text-Zone/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "af946713adc6107d8806",
+    "url": "/Text-Zone/static/js/main.c5590ea8.chunk.js"
+  },
+  {
+    "revision": "41ddca7204d8526272ef",
+    "url": "/Text-Zone/static/js/runtime~main.6da9b4d0.js"
+  }
+]);
